463318fc6b8312f6d8615f2d12a7ea4eaa3cb4f8
Дима <mamleev-02@mail.ru>
2020-06-20 06:30:45 +0300

    first commit

---
+++ Шутка.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using System.IO;
+
+using System.Net;
+using ConsoleApp25;
+using Newtonsoft.Json;
+
+namespace JokeApplication
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            //Ссылка на Api запрос
+
+            string url = "https://api.icndb.com/jokes/random";
+
+            string response;
+
+            //Отправляем запрос на сервер для получения JSON ответа
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            //Получаем ответ от сервера
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+
+            {
+
+                response = streamReader.ReadToEnd();
+
+            }
+
+            //Получаем из JSON после JOKE
+
+            ValueJSON valueJSON = JsonConvert.DeserializeObject<ValueJSON>(response);
+
+            Console.WriteLine("Шутка: {0}", valueJSON.Value.Joke);
+
+            //Чтобы программа сразу не закрывалась
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
